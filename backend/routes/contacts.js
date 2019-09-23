ebaa4797cc00876730d4618ffcad5029cadf47fc
Jess <jnoriegaless@gmail.com>
2019-09-23 11:58:53 +0000

    Add CRUD routes

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET contacts listing. */
+/* CRUD contacts listing. */
 module.exports = knex => {
   router.get('/', function(req, res, next) {
     knex
@@ -13,6 +13,35 @@ module.exports = knex => {
       .catch(error => {
         console.log(error);
       });
+  });
+
+  router.put("/:id", function(req, res, next) {
+    knex("contacts")
+      .where({ id: req.params.id })
+      .update(req.body.contacts)
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => console.log(error));
+  });
+
+  router.post("/", function(req, res, next) {
+    knex("contacts")
+      .insert(req.body.contacts, ["id"])
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => console.log(error));
+  });
+
+  router.delete("/:id", function(req, res, next) {
+    knex("contacts")
+      .where({ id: req.params.id })
+      .del()
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => console.log(error));
   });
 
   return router;
